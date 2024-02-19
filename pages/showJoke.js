d5ffb79ec0022f5d43fbeb184ef8e941d4e205f0
Jesse French <frejes32@gmail.com>
2024-02-18 18:59:03 -0600

    Met acceptance criteria for project

---
+++ pages/showJoke.js
@@ -0,0 +1,15 @@
+import renderToDom from '../utils/renderToDom';
+
+const showJoke = (joke) => {
+  let domString = '';
+  domString += `<h3>${joke.setup}</h3>`;
+  renderToDom('#joke', domString);
+};
+
+const showPunchline = (joke) => {
+  let domString = '';
+  domString += `<h3>${joke.delivery}</h3>`;
+  renderToDom('#punchline', domString);
+};
+
+export { showJoke, showPunchline };
